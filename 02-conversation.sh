fe657e43d12611de1ea9d6f60770257226436f27
Bavani Gowda <bhavani810527@gmail.com>
2026-01-04 23:50:02 +0530

    02 Conversation started

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh, I am good. How are you?"
+echo "Ramesh:: What are you learning recently?"
+echo "Suresh:: I am Learning Shell Scripting Ramesh"
