945bfccb4051508e38d9d9d692c5e266382e2283
Bavani Gowda <bhavani810527@gmail.com>
2026-01-06 19:47:13 +0530

    02-conversation.sh

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+
+
+echo "Bavani:: Welcome to singapore Bhuvi"
+echo "Bhuvi:: Thank you, wher are going now Bavani"
+echo "Bavani:: First will go to Airport Bhuvi, then will see"
+echo "Bhuvi:: Then yaa will start now bavani.
