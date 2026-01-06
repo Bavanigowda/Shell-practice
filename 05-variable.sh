e5d9bfcdca979ed1d0779b5fff2b48d1026adc7f
Bavani Gowda <bhavani810527@gmail.com>
2026-01-06 21:04:02 +0530

    varibles

--- 05-variable.sh
+++ 05-variable.sh
@@ -2,5 +2,7 @@
 
 echo " please enter your PIn number "
 read -s PIN Number # here PIN number is a variable 
+# here when we add read -s this will hide the passoword
+#echo should always be in lowercase it should not be ECHO
 
 echo "PIN entered is: $PIN_NUMBER"
