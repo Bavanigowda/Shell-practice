42fb3cdf7303bf3b06e934b7e5bffd4c281998fa
Bavani Gowda <bhavani810527@gmail.com>
2026-01-25 12:34:10 +0530

    variables

--- 07-variable.sh
+++ 07-variable.sh
@@ -4,5 +4,16 @@ Date =$date
 
 echo "timestamp executed =$date"
 
+Start_Time=$(date +%s)
+
+Sleep 10 &
+
+End_time=$(date +%s)
+
+Total_Time=$($end_time-$start-time)
+
+echo "Script executed in: $Total_Time seconds"
+
+
 
 
