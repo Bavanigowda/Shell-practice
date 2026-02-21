85a5b852e2c768b9d57bda043fc9d02b321e6318
Bavani Gowda <bhavani810527@gmail.com>
2026-02-21 18:20:17 +0530

    Add 16-logs.sh script

---
+++ 16-logs.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+USERID=$(id  -u)
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG-FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)"
+
+
+if [ $USERID -ne 0 ]; then
+   echo "Error:: please run this script with root priviledge"
+   exit 1 # failure is other than 
+   fi
+
+   VALIDATE(){  # functions received inputs through args like shell scripting
+   dnf install mysql -y &>>$LOG_FILE
+
+   if [ $1 -ne 0 ]; then
+      echo -e "Installing $2 ... $R failure $N"
+      exit 1 # failure is other than 
+    else 
+      echo -e "Installing $2 ... $G success $N"
+      exit 1 # failure is other than 
+    fi
+}
+
+dnf list installed mysql &>>$LOG_FILE
+#install if it is not found
+if [ $? -ne 0 ]; then
+    dnf insatll mysql -y &>>$LOG_FILE
+    VALIDATE $? "Mysql"
+else
+echo "Mysql already exist ... $y SKIPPING $N"
+fi
+
+dnf list installed nginx &>>$LOG_FILE
+#install if it is not found
+if [ $? -ne 0 ]; then
+    dnf install nginx -y &>>$LOG_FILE
+    VALIDATE $? "nginx"
+else
+echo "Nginix already exist ... $G SKIPPING $N"
+fi
+
